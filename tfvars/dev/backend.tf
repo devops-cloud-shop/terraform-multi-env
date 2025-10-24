9418045fdaf2351620110ac8374ae02c4a630bec
Prav-Akula <pravs.akula@outlook.com>
2025-10-24 18:39:12 -0400

    terraform-multi-env

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,5 +1,9 @@
-bucket = "remote-state-86s-multi-env-dev"
-key    = "tfvars-dev"
-region = "us-east-1"
-use_lockfile = true
-encrypt = true
+terraform {
+    backend "s3" {
+        bucket = "remote-state-86s-multi-env-dev"
+        key    = "tfvars-dev"
+        region = "us-east-1"
+        use_lockfile = true
+        encrypt = true
+ }
+}
--- tfvars/prod/backend.tf
+++ tfvars/prod/backend.tf
@@ -1,5 +1,9 @@
-bucket = "remote-state-86s-multi-env-prod"
-key    = "tfvars-prod"
-region = "us-east-1"
-use_lockfile = true
-encrypt = true
+terraform {
+    backend "s3" {
+        bucket = "remote-state-86s-multi-env-prod"
+        key    = "tfvars-prod"
+        region = "us-east-1"
+        use_lockfile = true
+        encrypt = true
+    }
+}
